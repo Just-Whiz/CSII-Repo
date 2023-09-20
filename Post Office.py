42ab147b4ca00ba0f7506b7404f4290200b2eea4
Just-Whiz <christiansuy25@gmail.com>
2023-09-20 14:13:15 -0400

    defining the functions

--- Post Office.py
+++ Post Office.py
@@ -1,7 +1,7 @@
 #Project name: GCDS Rust Removal AKA the GCDS Post office algorithm
 #Started: 9/18/23
 #Ended: 
-#Current version: 0.191823
+#Current version: 0.21
 #Working on: pseudocode and getting some necessary variables down
 
 import random
@@ -10,6 +10,10 @@ import time
 l = 0
 h = 0
 t = 0
+
+def getzone(zip):
+    if zonenumber
+    return zone
 
 def main():
     print("Welcome to the post office. Below, please enter the dimensions of the postage you plan to send.")
@@ -21,7 +25,9 @@ def main():
     zip2 = input("Enter your ending ZIP code adress: ")
 
 
-def classify():
+def zipcode(): 
+
+def getsize(l, h t):
     if l >= 3.5 or l <= 4.25 and h >= 3.5 or h <= 6 and t >= 0.007 or t <= 0.016:
         postclass == "Regular Post Card"
     elif l >= 4.25 or l <= 6 and h >= 6 or h <= 11.5 and t >= 0.007 or t <= 0.15:
@@ -30,6 +36,7 @@ def classify():
         postclass == "Envelope"
     elif l >= 6.125 or l <= 24 and h >= 11 or h <= 18 and t >= 0.25 or t <= 0.5:
         postclass == "Large Envelope"
+    return post class
 
 while True:
     main()
