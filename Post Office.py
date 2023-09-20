19d6462cd195400283f78e3b7a767833bff177ae
Just-Whiz <christiansuy25@gmail.com>
2023-09-20 14:43:40 -0400

    cs ii time changes

--- Post Office.py
+++ Post Office.py
@@ -2,41 +2,34 @@
 #Started: 9/18/23
 #Ended: 
 #Current version: 0.21
+#Challenges: 
 #Working on: pseudocode and getting some necessary variables down
 
-import random
 import time
+import os
 
 l = 0
 h = 0
 t = 0
+zip1 = 0
+zip2 = 0
+zonenumber = 0
 
-def getzone(zip):
-    if zonenumber
-    return zone
+def getsize(l, h, t):
+    if (l >= 3.5 or l <= 4.25) and (h >= 3.5 or h <= 6) and (t >= 0.007 or t <= 0.016): postclass == "Regular Post Card"                #If the length is between 
+    elif (l >= 4.25 or l <= 6) and (h >= 6 or h <= 11.5) and (t >= 0.007 or t <= 0.15): postclass == "Large Post Card"
+    elif (l >= 3.5 or l <= 6.125) and (h >= 5 or h <= 11.5) and (t >= 0.25 or t <= 0.5): postclass == "Envelope"
+    elif (l >= 6.125 or l <= 24) and (h >= 11 or h <= 18) and (t >= 0.25 or t <= 0.5): postclass == "Large Envelope"
+    elif (l > and t >  and h > ):
+        if
+        else
+    return postclass
+
+def getzone():
+    if (zip1 and zip2 >= 1 or zip1 and zip2 <= 6999): zonenumber = 1
+    elif (zip1 and zip2 >= )
+
+    return zonenumber
+
 
 def main():
-    print("Welcome to the post office. Below, please enter the dimensions of the postage you plan to send.")
-    time.sleep(1)
-    l = input("Enter your postage's length: ")
-    h = input("Enter your postage's height: ")
-    t = input("Enter your postage's thickness: ")
-    zip1 = input("Enter your starting ZIP code address: ")
-    zip2 = input("Enter your ending ZIP code adress: ")
-
-
-def zipcode(): 
-
-def getsize(l, h t):
-    if l >= 3.5 or l <= 4.25 and h >= 3.5 or h <= 6 and t >= 0.007 or t <= 0.016:
-        postclass == "Regular Post Card"
-    elif l >= 4.25 or l <= 6 and h >= 6 or h <= 11.5 and t >= 0.007 or t <= 0.15:
-        postclass == "Large Post Card"
-    elif l >= 3.5 or l <= 6.125 and h >= 5 or h <= 11.5 and t >= 0.25 or t <= 0.5:
-        postclass == "Envelope"
-    elif l >= 6.125 or l <= 24 and h >= 11 or h <= 18 and t >= 0.25 or t <= 0.5:
-        postclass == "Large Envelope"
-    return post class
-
-while True:
-    main()
