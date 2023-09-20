c54f6b2f05bb8e08b65abb4ebb9ad9683e061b76
Just-Whiz <christiansuy25@gmail.com>
2023-09-20 14:46:37 -0400

    finished making the first part of getsize

--- Post Office.py
+++ Post Office.py
@@ -6,23 +6,19 @@
 #Working on: pseudocode and getting some necessary variables down
 
 import time
-import os
 
 l = 0
 h = 0
 t = 0
 zip1 = 0
 zip2 = 0
-zonenumber = 0
 
 def getsize(l, h, t):
     if (l >= 3.5 or l <= 4.25) and (h >= 3.5 or h <= 6) and (t >= 0.007 or t <= 0.016): postclass == "Regular Post Card"                #If the length is between 
     elif (l >= 4.25 or l <= 6) and (h >= 6 or h <= 11.5) and (t >= 0.007 or t <= 0.15): postclass == "Large Post Card"
     elif (l >= 3.5 or l <= 6.125) and (h >= 5 or h <= 11.5) and (t >= 0.25 or t <= 0.5): postclass == "Envelope"
     elif (l >= 6.125 or l <= 24) and (h >= 11 or h <= 18) and (t >= 0.25 or t <= 0.5): postclass == "Large Envelope"
-    elif (l > and t >  and h > ):
-        if
-        else
+    #elif (l > 6.125 and h > 18  and t > 0.5):
     return postclass
 
 def getzone():
