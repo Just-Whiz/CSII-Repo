18beaf334c0a8d5a33bbbf068028120cb547cf21
Just-Whiz <christiansuy25@gmail.com>
2023-09-20 14:48:58 -0400

    1st lines of documentation for getsize()

--- Post Office.py
+++ Post Office.py
@@ -14,12 +14,15 @@ zip1 = 0
 zip2 = 0
 
 def getsize(l, h, t):
-    if (l >= 3.5 or l <= 4.25) and (h >= 3.5 or h <= 6) and (t >= 0.007 or t <= 0.016): postclass == "Regular Post Card"                #If the length is between 
+    #Function takes in 3 parameters (length, height, and thickness [l, h, & t])
+    #If the parameters are between certain numbers and all of them meet the parameters, then the variable "postclass" will be changed dependent on the parameter given.
+    #Once the calculations are performed, the function finishes by returning the value of postclass, which was set by the parameters
+    if (l >= 3.5 or l <= 4.25) and (h >= 3.5 or h <= 6) and (t >= 0.007 or t <= 0.016): postclass == "Regular Post Card"                
     elif (l >= 4.25 or l <= 6) and (h >= 6 or h <= 11.5) and (t >= 0.007 or t <= 0.15): postclass == "Large Post Card"
     elif (l >= 3.5 or l <= 6.125) and (h >= 5 or h <= 11.5) and (t >= 0.25 or t <= 0.5): postclass == "Envelope"
     elif (l >= 6.125 or l <= 24) and (h >= 11 or h <= 18) and (t >= 0.25 or t <= 0.5): postclass == "Large Envelope"
     #elif (l > 6.125 and h > 18  and t > 0.5):
-    return postclass
+    return postclass 
 
 def getzone():
     if (zip1 and zip2 >= 1 or zip1 and zip2 <= 6999): zonenumber = 1
