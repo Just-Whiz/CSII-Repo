14fd4a6ef75d65c7e60cd2e329d40c753cd2329e
Just-Whiz <christiansuy25@gmail.com>
2023-09-22 13:47:31 -0400

    figuring out zip stuff uggghhhhh

--- Post Office.py
+++ Post Office.py
@@ -21,15 +21,15 @@ def main():
     time.sleep(2)
     print("")
 
+
 # Using the special variable 
 # __name__
 if __name__=="__main__":
     main()
 
-
 #Function takes in 3 parameters (length, height, and thickness [l, h, & t])
 #If the parameters are between certain numbers and all of them meet the parameters, then the variable "postclass" will be changed dependent on the parameter given.
-#Once the calculations are performed, the function finishes by returning the value of postclass, which was set by the parameters
+#Once the calculations are performed, the function finishes by returning the value of postclass, determined by the parameters
 
 def getsize(l, h, t):
     if (l >= 3.5 or l <= 4.25) and (h >= 3.5 or h <= 6) and (t >= 0.007 or t <= 0.016): postclass = "Regular Post Card"                
@@ -40,3 +40,4 @@ def getsize(l, h, t):
     #elif (l > 6.125 and h > 18  and t > 0.5): [working on the next set of parameters]
 
     return postclass
+
