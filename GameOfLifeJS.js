fdc883e761255c176708b820071f2d3fd818ba01
Just-Whiz <christiansuy25@gmail.com>
2023-10-03 20:12:04 -0400

    ae

---
+++ GameOfLifeJS.js
@@ -0,0 +1,21 @@
+// :When the size is submitted by the user, call the loadGrid() function to generate the grid
+// based on those parameters
+
+function loadGrid() {
+    let tbl = document.getElementById("gridCanvas")
+    
+    for(let i= 0; i < 11; i++) {
+        let myRow = document.createElement("tr")
+        myRow.id = "row" + i;
+
+        tbl.appendChild(myRow);
+        let rowW = document.getElementById("row" + i);
+
+        for(let j= 0; j < 11; j++) {
+            let myCell = document.getElementById("td");
+
+            rowW.appendChild(myCell)
+        }
+    }
+}
+
