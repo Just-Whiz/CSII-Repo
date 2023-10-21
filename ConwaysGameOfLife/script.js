f64a592bb09c5fc29ab30ab8765cb4c1daf4e2f9
Just-Whiz <christiansuy25@gmail.com>
2023-10-21 09:24:08 -0400

    making divs work

---
+++ ConwaysGameOfLife/script.js
@@ -0,0 +1,9 @@
+const BtnAdd = document.querySelector(".btn-add");
+
+BtnAdd.addEventListener.addEventListener("click", AddNew)
+
+function Addnew() {
+    const newDiv = document.createElement("div")
+    console.log("add")
+    document.body.appendChild(newDiv)
+}
