a0cd102fa2269ecb8237c2353ff8097079012cc2
Just-Whiz <christiansuy25@gmail.com>
2023-10-24 09:55:21 -0400

    fixed more js

--- ConwaysGameOfLife/script.js
+++ ConwaysGameOfLife/script.js
@@ -5,5 +5,6 @@ BtnAdd.addEventListener.addEventListener("click", AddNew)
 function Addnew() {
     const newDiv = document.createElement("div")
     console.log("add")
+    newDiv 
     document.body.appendChild(newDiv)
 }
