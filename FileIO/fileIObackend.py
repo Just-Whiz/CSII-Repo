46067d8dd82e9fd2b2d664fa8887fa501f2212c1
Just-Whiz <christiansuy25@gmail.com>
2023-11-07 12:00:08 -0500

    csv data got moited

--- FileIO/fileIObackend.py
+++ FileIO/fileIObackend.py
@@ -9,5 +9,4 @@ with open("C:\\Users\\csuy26\Desktop\\CSII-Repo\\FileIO\\gcds_data3.csv", 'r') a
         student_last_name = row[1]
         student_year = row[2]
         student_grade = row[3]
-        line = "{},{}\n".format(student_first_name, student_last_name)
-        outfile.write(line)
+        print(student_first_name, student_last_name, student_year, student_grade)
