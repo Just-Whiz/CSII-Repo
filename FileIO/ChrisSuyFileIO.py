79ef50bd760a318808b2c6f85491a339199ae1e6
Just-Whiz <christiansuy25@gmail.com>
2023-11-20 21:23:30 -0500

    Working on the simple selections function

--- FileIO/ChrisSuyFileIO.py
+++ FileIO/ChrisSuyFileIO.py
@@ -20,9 +20,11 @@ def main():
    
     while go is True:
         print('''Menu: Enter Choice or 'Q' to (Q)uit:
+              
 1) Print all students at school
 2) Print all students in grade 12
 3) Compare the amount of boys versus girls
+4) Make a selection based on available data
 ''')
         time.sleep(1)
         answer = input("Enter your input here: ")
@@ -35,6 +37,9 @@ def main():
             time.sleep(1)
         elif answer == "3":
             compare_genders(file_input)
+        elif answer == "4":
+            make_simple_selections(file_input)
+        elif answer == ""
 
 
 
@@ -56,29 +61,31 @@ def display_all(file_input):
     for record in file_input: 
         row = record.split(",")
         time.sleep(0.00000000001)
-        print(row[1], row[2], row[3], row[4], row[5])
+        print(row[1], row[0], row[2], row[3], row[4], row[5])
 
 def count_seniors(file_input):
     """
     This function displays all the seniors' names that currently go to GCDS. 
 
-    By taking file_input as a variable, (as defined in the main), this function is able to iterate through all the kids (rows)
+    By taking file_input as a variable, (as defined in the main), this function is able to iterate through all the rows
     in the csv file that the variable is attached to (gcds_data3.csv), and single out the first and last kids of the 
     kids who are in that grade level. After each name is listed, the iteration pauses for a fraction of a second before
     continuing. 
 
     record - Denotes the columns 
-    kid - Denotes the columns and rows and splits them them by the delimiter (in this case, a comma ",")
+    row - Denotes the columns and rows and splits them them by the delimiter (in this case, a comma ",")
     """
     
     file_input.seek(1)                                     #moves pointer to line 1
 
+    print("The kids in 12th grade are: ")
+
     for record in file_input:
-        kid = record.split(",")
-        if kid[2] == "Grade 12":
+        row = record.split(",")
+        if row[2] == "Grade 12":
             time.sleep(0.01)
-            print(kid[1] + " " + kid[0])
-        time.sleep(1)
+            print(row[1] + " " + row[0])
+    time.sleep(1)
 
 def compare_genders(file_input):
     """
@@ -106,6 +113,18 @@ def compare_genders(file_input):
     elif females_counter > males_counter:
         print("there are more gals")
     time.sleep(1)
+
+def make_simple_selections(file_input):
+    """
+    This function allows the user to make simple selections based on 5 choices: Find 
+    """
+
+    file_input.seek(1)
+
+    for record in file_input:
+        row = record.split(",")
+        record.sort()
+        
 
 if __name__ == '__main__':
     main()
