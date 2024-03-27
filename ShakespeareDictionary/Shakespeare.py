418aa509361c545d71bcd232e0b0de023135a642
Chris Suy <christiansuy25@gmail.com>
2024-03-27 13:44:23 -0400

    Fixed txt file access

--- ShakespeareDictionary/Shakespeare.py
+++ ShakespeareDictionary/Shakespeare.py
@@ -1,34 +1,44 @@
 from string import punctuation
-from pathlib import Path
-import csv
-from time import sleep
+import time
 
 def main():
     """
+    This is the main function of the 
+
 
     """
 
-    print("""Here are your choices for graphing the word usage in Shakespeare:
-1): Macbeth
-2): A Midsummer Night's Dream
-          """)
-    choice = input("What is your choice? ")
-    if choice == 1 or "1":
-        current_dir = Path(__file__).parent
-        file_path = current_dir / "Macbeth.txt"
-        dictionary = count_words(file_path)
-        print(dictionary)
-    elif choice == 2 or "1": 
-        current_dir = Path(__file__).parent
-        file_path = current_dir / "Midsummer.txt"
-        print(f"You are doing Macbeth")
-        count_words(file_path)
+    counted_dictionary = ""
+    try:
+        print("""Here are your choices for graphing the word usage in Shakespeare:
+    1): Macbeth
+    2): A Midsummer Night's Dream
+            """)
+        choice = input(int("What is your choice? "))
+        if choice == 1 or "1":
+            current_path = "Macbeth.txt"
+            print("You are doing a Midsummer Night's Dream")
+            with open(current_path):
+                counted_dictionary = count_words(current_path)
+                print(counted_dictionary)
+        elif choice == 2 or "2": 
+            current_path = "Midsummer.txt"
+            print("You are doing A Midsummer Night's Dream")
+            with open("current_path"):
+                print(f"You are doing A Midsummer Night's Dream")
+                counted_dictionary = count_words(counted_dictionary)
+    except ValueError:
+        print("Please enter a number value.")
+        time.sleep(1)
+                
 
 
 
 
 def count_words(current_path):
-
+    """
+    
+    """
     word_count = {}
 
     with open(current_path) as file:
