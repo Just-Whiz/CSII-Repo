57e1b6b29aa2429e6568ba00ac76bd084c444639
Chris Suy <christiansuy25@gmail.com>
2024-03-28 13:36:51 -0400

    Reorganized everything into a main

--- ShakespeareDictionary/Shakespeare.py
+++ ShakespeareDictionary/Shakespeare.py
@@ -1,47 +1,33 @@
 from string import punctuation
+import plotly.graph_objects as go
 import time
 
 def main():
     """
-    This is the main function of the 
-
-
-    """
-
-    counted_dictionary = ""
-    try:
-        while True:
-            current_path = ""
-            print("""Here are your choices for graphing the word usage in Shakespeare:
-        1): Macbeth
-        2): A Midsummer Night's Dream
-                """)
-            choice = input("What is your choice? Please enter it here: ")
-            choice = int(choice)
-            if choice == 1 or "1":
-                current_path = "Macbeth.txt"
-                print("You are doing a Midsummer Night's Dream")
-                with open(current_path, "r") as f:
-                    counted_dictionary = count_words(current_path)
-                    print(counted_dictionary)
-                    continue
-    except ValueError:
-        print("Please enter a number value.")
-        time.sleep(1)
-                
-
-def count_words(current_path):
-    """
     
     """
-    word_count = {}
+    num_results = 20
 
-    with open(current_path) as file:
+    counts = {}
+
+    current_file = input("What is your choice?")
+    if current_file == 1:
+        current_file = "Macbeth.txt"
+    elif current_file == 2:
+        current_file = "Midsummer.txt"
+
+    with open("stopwords.txt") as file:
+        stopwords = {line.strip() for line in file}
+
+    with open(current_file) as file:
         for line in file:
             for word in line.split():
                 cleaned = word.lower().strip(punctuation)
-                word_count[cleaned] = word_count.get(word, 0) + 1
-    return word_count
+                if cleaned not in stopwords:
+                    counts[cleaned] = counts.get(cleaned, 0) + 1
+    for word in sorted(counts, key=counts.get, reverse=True)[:num_results]:
+        print(f"{word}\t:{counts[word]}")
+    
 
 if __name__ == "__main__":
     main()
