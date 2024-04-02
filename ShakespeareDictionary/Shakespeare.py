d058e9103ff6280bab7c1f2609fa77c608e2f353
Chris Suy <christiansuy25@gmail.com>
2024-04-02 10:13:35 -0400

    Isolated counting words for Macbeth and Midsummer seperately

--- ShakespeareDictionary/Shakespeare.py
+++ ShakespeareDictionary/Shakespeare.py
@@ -1,5 +1,4 @@
 from string import punctuation
-import plotly.graph_objects as go
 import time
 
 def main():
@@ -10,16 +9,21 @@ def main():
 
     counts = {}
 
-    current_file = input("What is your choice?")
-    if current_file == 1:
-        current_file = "Macbeth.txt"
-    elif current_file == 2:
-        current_file = "Midsummer.txt"
+    print("""
+1) Macbeth
+2) Midsummer
+""")
+    choice = input("What would you like to plot? Enter here: ")
+    if choice == "1":
+        count_macbeth_words(counts, num_results, "stopwords.txt", "Macbeth.txt")
+    elif choice == "2":
+        count_midsummer_words(counts, num_results)
 
-    with open("stopwords.txt") as file:
+def count_macbeth_words(counts, num_results, stopwords_file, pathway):
+    with open(stopwords_file) as file:
         stopwords = {line.strip() for line in file}
 
-    with open(current_file) as file:
+    with open(pathway) as file:
         for line in file:
             for word in line.split():
                 cleaned = word.lower().strip(punctuation)
@@ -27,7 +31,22 @@ def main():
                     counts[cleaned] = counts.get(cleaned, 0) + 1
     for word in sorted(counts, key=counts.get, reverse=True)[:num_results]:
         print(f"{word}\t:{counts[word]}")
-    
+
+
+
+def count_midsummer_words(counts, num_results):
+# feijwa
+    with open("stopwords.txt") as file:
+        stopwords = {line.strip() for line in file}
+
+    with open("Midsummer.txt") as file:
+        for line in file:
+            for word in line.split():
+                cleaned = word.lower().strip(punctuation)
+                if cleaned not in stopwords:
+                    counts[cleaned] = counts.get(cleaned, 0) + 1
+    for word in sorted(counts, key=counts.get, reverse=True)[:num_results]:
+        print(f"{word}\t:{counts[word]}")
 
 if __name__ == "__main__":
     main()
