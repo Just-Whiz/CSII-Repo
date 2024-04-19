c114368bbab47c93c10f94406f5f3971a8586d82
Just-Whiz <christiansuy25@gmail.com>
2024-04-19 14:04:13 -0400

    Got the gameboard working

--- Battleship/ChrisSuyBattleShip.py
+++ Battleship/ChrisSuyBattleShip.py
@@ -7,26 +7,40 @@
 
 import time
 import sys
+import pygame
 import random
 
+pygame.init()
+
+#my_sound = pygame.mixer.Sound("Path/to/my/soundfile.wav")
+#my_sound.play()
 
 def main():
     print("Welcome to Battleship!")
-    generateArray()
+    setUpGame()
 
-def generateArray():
-    rows, cols = (10, 10)
-
-    arr = [[0 for i in range(cols)] for j in range(rows)]
-    for row in arr:
+def setUpGame():
+    gameBoard = generateArray()
+    for row in gameBoard:                                                                                 # Prints all values in the array out
         print(row)
 
-def print1by1(text, delay=0.015):
-    for c in text:
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        time.sleep(delay)
-    print
+
+def generateArray():
+    """
+    This function generates gameboard arrays. 
+
+    First, it assigns rows and cols a value of 10 (to create a 10 by 10 grid) in order to create an array/grid.
+    Then, the array (referenced as the variable arr) is created with two for loops, one to create the columns of the array,
+    and the other to create the rows, all filled with 0's to represent 
+
+    Local Variables:
+
+    """
+    rows, cols = (10, 10)
+
+    arr = [[0] * cols for i in range(rows)]
+    return arr
+
 
 if __name__ == "__main__":
     main()
